ab3c245895f4126726334f7ad8ae9f94eb656645
bro-panel <imdbro7@gmail.com>
2025-08-15 15:00:59 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js');
-const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const pastebin = new PastebinAPI('hvQLxEHI45T4iKjVasDz0UEriGvVMP-k');
 const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
